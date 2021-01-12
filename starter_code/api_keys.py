ca1e85801faf09e6e14db2bbd799dcecbc7961dd
qizhengyaoyao <28369996+qizhengyaoyao@users.noreply.github.com>
2021-01-12 14:34:12 +1100

    WeatherPy coding finished

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "7e0d199aaf74fe2ddeb9a06a2648da81"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBnbnuSlbRKs5acEZB-Ta21AcLUoLVqaEM"
